c4f9d6717198e056880148044fa8d1bf49dbc44e
Lucas <lucas@agtlucas.com>
2015-09-26 20:05:28 -0300

    Updates

--- app/src/main/java/ninja/lucas/blurrr/MainActivity.java
+++ app/src/main/java/ninja/lucas/blurrr/MainActivity.java
@@ -1,16 +1,11 @@
 package ninja.lucas.blurrr;
 
-import android.app.Activity;
-import android.media.Image;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
 import android.animation.Animator;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
-import android.support.v7.app.*;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.view.animation.OvershootInterpolator;
 import android.widget.ImageView;
@@ -68,26 +63,4 @@ public class MainActivity extends ActionBarActivity {
 //            }
 //        }
 //    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
