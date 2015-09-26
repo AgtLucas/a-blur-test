e876f1c1b24b21b2f52e4738992b47f6beca2b5f
Lucas <lucas@agtlucas.com>
2015-09-26 19:39:29 -0300

    Update MainActivity

--- app/src/main/java/ninja/lucas/blurrr/MainActivity.java
+++ app/src/main/java/ninja/lucas/blurrr/MainActivity.java
@@ -1,6 +1,7 @@
 package ninja.lucas.blurrr;
 
 import android.app.Activity;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,13 +19,55 @@ import com.fivehundredpx.android.blur.BlurringView;
 
 import java.util.Random;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBlurringView = (BlurringView) findViewById(R.id.blurring_view);
+        View blurredView = findViewById(R.id.blurring_view);
+
+        mBlurringView.setBlurredView(blurredView);
+
+        mImageViews[0] = (ImageView) findViewById(R.id.image0);
+        mImageViews[1] = (ImageView) findViewById(R.id.image1);
+        mImageViews[2] = (ImageView) findViewById(R.id.image2);
+        mImageViews[3] = (ImageView) findViewById(R.id.image3);
+        mImageViews[4] = (ImageView) findViewById(R.id.image4);
+        mImageViews[5] = (ImageView) findViewById(R.id.image5);
+        mImageViews[6] = (ImageView) findViewById(R.id.image6);
+        mImageViews[7] = (ImageView) findViewById(R.id.image7);
+        mImageViews[9] = (ImageView) findViewById(R.id.image8);
     }
+
+    private BlurringView mBlurringView;
+
+    private int[] mImageIds = {
+        R.drawable.p0, R.drawable.p1, R.drawable.p2, R.drawable.p3, R.drawable.p4, R.drawable.p5, R.drawable.p6, R.drawable.p7, R.drawable.p8, R.drawable.p9
+    };
+
+    private ImageView[] mImageViews = new ImageView[9];
+    private int mStartIndex;
+
+    public void shuffle(View view) {
+
+    }
+
+    private ValueAnimator.AnimatorUpdateListener listener = new ValueAnimator.AnimatorUpdateListener() {
+        @Override
+        public void onAnimationUpdate(ValueAnimator animation) {
+            mBlurringView.invalidate();
+        }
+    };
+
+//    public void shift(View view) {
+//        if (!mShifted) {
+//            for (ImageView imageView : mImageViews) {
+//                ObjectAnimator tx = ObjectAnimator.ofFloat()
+//            }
+//        }
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
